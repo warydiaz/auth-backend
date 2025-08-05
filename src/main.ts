df06b86632ba04432c621b4979b19063c2f96a69
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2025-08-05 18:03:46 +0200

    feat: add CORS configuration with dynamic origin and credentials in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.URL!,
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'https://nextest.santitorrabadella.com',
+        'http://localhost:3000',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
   await app.listen(process.env.PORT!);
