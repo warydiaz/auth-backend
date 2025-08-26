cea50c0f31e997b3765ef58eb7c7ddde107f3f54
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2025-08-26 08:37:39 +0200

    fix: simplify CORS configuration by using a static origin array

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'https://nextest.santitorrabadella.com',
-        'http://localhost:3000',
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: ['https://nextest.santitorrabadella.com/', 'http://localhost:80/'],
     credentials: true,
   });
   await app.listen(process.env.PORT!);
